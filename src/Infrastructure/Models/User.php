e43f01aa46edbe96577202821f3d6de40259a8ca
XpnsiveSharks <aban.marynelle.tesoro@gmail.com>
2025-09-09 16:06:28 +0800

    feat(user): implement UUID support in User model and repository

--- src/Infrastructure/Models/User.php
+++ src/Infrastructure/Models/User.php
@@ -1,19 +1,26 @@
 <?php
+
 namespace App\Infrastructure\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class User extends Model
 {
     use SoftDeletes;
 
     protected $table = 'users';
-    public $incrementing = false; // since `id` is VARCHAR
+    public $incrementing = false;
     protected $keyType = 'string';
 
     protected $fillable = [
-        'id', 'name', 'email', 'password', 'role', 'phone'
+        'id',
+        'name',
+        'email',
+        'password',
+        'role',
+        'phone'
     ];
 
     // Relationships
